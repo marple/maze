20efc912d5c9a3e9702b5d30bcf9ebd9c1ac84ff
emiliedpm <emiliedupetitmagneux@gmail.com>
2017-10-20 14:29:49 +0200

    Ajout de méthodes dans la classe maze et ajout de commentaires

--- maze_1.py
+++ maze_1.py
@@ -1,17 +1,11 @@
 #recursive backtracking algorithm
 
 import random
-import pygame
-from pygame.locals import *
-
-pygame.init()
-
-#screen = pygame.display.set_mode((500, 500))
-title = pygame.display.set_caption("Mac Gyver VS the guard")
 
 
-class Maze:  #faut-il ajouter une méthode pour calculer le nombre total de cases du labyrinthe??
-    #ou un attribut nombre de cases dans le constructeur?
+
+class Maze:
+
     """
     description
     """
@@ -25,14 +19,14 @@ class Maze:  #faut-il ajouter une méthode pour calculer le nombre total de case
     def generate_grid(self):
             for j in range(0, self.cols):
                  for i in range(0, self.rows):
-                    self.grid.append(Cell(i,j)) #erreur car on fait appel à l'objet cell en dehors de la classe
+                    self.grid.append(Cell(i,j))
 
     def index(self, i, j):
         """This method calculates the index of the current cell"""
         index = i + j * self.cols
         return self.grid[index]
 
-    def checkNeighbors(self,current_cell):  #dans quelle classe je mets cette fonction?
+    def checkNeighbors(self,current_cell):
         """
             Checks if the neighbors cells have been visited
         """
@@ -56,27 +50,51 @@ class Maze:  #faut-il ajouter une méthode pour calculer le nombre total de case
         if left and not left.visited:
             neighbors.append(left)
 
-        if len(neighbors)>0:
-            r = random.randint(0, len(neighbors)-1)
+        if len(neighbors)>0: #if there are neighbors wich have not been visited
+            r = random.randint(0, len(neighbors)-1) #chooses a neighbor randomly
             print('r',str(r))
             return neighbors[r]
         else:
             return None
 
-    #def display(self, window):
-    #    """This method displays the maze according to the grid"""
-    def draw(self):
+    def remove_walls(self, a, b):
+      """Remove the wall between the current cell(a) and the next cell(b)"""
+      x = a.self.i - b.self.i
+      if x == 1:
+          a.walls[3] = False #remove the left wall of the current cell
+          b.walls[1] = False #remove the right wall of the next cell
+      elif x == -1:
+          a.walls[1] = False #remove the right wall of the current cell
+          b.walls[3] = False #remove the left wall of the next cell
+
+      y = a.self.j - b.self.j
+      if y == 1:
+          a.walls[0] = False #remove the top wall of the current cell
+          b.walls[2] = False #remove the bottom wall of the next cell
+      elif y == -1:
+          a.walls[2] = False #remove the bottom wall of the current cell
+          b.walls[0] = False #remove the top wall of the next cell
+
+    def create_path(self):
         """
-            Draw across the grid the path for this maze (à corriger)
+            Makes a path through the grid
         :return:nothing
         """
+        # TODO: il faut itérer tant qu'il reste des Cells non visitées
+       # on prend le premier objet Cell dans la liste grid
+        current = self.grid[0]
+       # on initialise la pile par une liste vide
+        stack = []
+        # on récupère la cellule voisine de la cellule courante
+        chosen_cell = self.checkNeighbors(current)
+        chosen_cell.visited = True
+        stack.append(current) #adds the current_cell to the stack
+        self.remove_walls(current, chosen_cell)
+        # Step 3 ok reste step 4
+        chosen_cell = current_cell
 
 
-
-
-
-
-class Cell:  #TODO:ajouter current, next?? current_cell = maze.grid[0]?
+class Cell:
     """
     class
     """
@@ -87,44 +105,14 @@ class Cell:  #TODO:ajouter current, next?? current_cell = maze.grid[0]?
         self.walls = [True, True, True, True]  #each cell has 4 walls
         self.visited = False
 
-    #def index(self):
-        #if self.i < 0 or self.j < 0 or self.i > Grid.cols - 1 or self.j > Grid.rows - 1:
-        #    return -1
-        #else:
-        #    return i + j * Grid.cols
-
 
     def highlight(self):
         x = self.i * self.width
         y = self.j * self.width
 
-    def remove_walls(self, a, b):
-        """Remove the wall between the current cell(a) and the next cell(b)"""
-        x = a.self.i - b.self.i
-        if x == 1:
-            a.walls[3] = False #remove the left wall of the current cell
-            b.walls[1] = False #remove the right wall of the next cell
-        elif x == -1:
-            a.walls[1] = False #remove the right wall of the current cell
-            b.walls[3] = False #remove the left wall of the next cell
-
-        y = a.self.j - b.self.j
-        if y == 1:
-            a.walls[0] = False #remove the top wall of the current cell
-            b.walls[2] = False #remove the bottom wall of the next cell
-        elif y == -1:
-            a.walls[2] = False #remove the bottom wall of the current cell
-            b.walls[0] = False #remove the top wall of the next cell
-
-        #if self.visited:
-            #TODO ajouter une fonction "graphique" pour effacer les murs
-
-            #TODO : ajouter la notion de stack : dans quelle classe?
-
-#class Dedale : classe qui génére l'application graphique du labyrinthe
 
 
-# pour test des classes et methodes
+# pour tester les classes et méthodes
 maze = Maze(15, 15, 2)
 maze.generate_grid()
 print('longueur de grid {}'.format(len(maze.grid)))
@@ -132,9 +120,9 @@ current_cell = maze.grid[0]
 print('current cell -> i :{} , j :{}'.format(current_cell.i, current_cell.j))
 current_cell2 = maze.grid[125]
 print('current cell2 -> i :{} , j :{}'.format(current_cell2.i, current_cell2.j))
-current = maze.index(5, 8)
-print('current -> i :{} , j :{}'.format(current.i, current.j))
-chosen_cell = maze.checkNeighbors(current)
+current3 = maze.index(5, 8)
+print('current -> i :{} , j :{}'.format(current3.i, current3.j))
+chosen_cell = maze.checkNeighbors(current3)
 print('chosen_cell -> i :{} , j :{}'.format(chosen_cell.i, chosen_cell.j))
 
 
